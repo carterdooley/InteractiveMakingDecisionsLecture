66650c651d2e5e8a58d84093629f27b99fb3de97
carter dooley <carter.j.dooley43@gmail.com>
2021-10-13 12:47:25 -0500

    Fighting begging

--- VarsandConditionals.js
+++ VarsandConditionals.js
@@ -1,1 +1,30 @@
-//Code: dfgdfgdfgdfgd
+//Code: 
+let johnSnowAttack = 25
+let jamieLannisterAttack = 35 
+
+if (johnSnowAttack > jamieLannisterAttack) {
+    console.log("John Snow has a better Attack")
+} else if (johnSnowAttack === jamieLannisterAttack) {
+    console.log("John and Jaime have the same Attack")
+} else {
+    console.log("Jaime Lannister has a better Attack")
+}
+
+let johnSnowHealth = 100
+let johnSnowDefense = 0
+
+if (johnSnowHealth <= jamieLannisterAttack) {
+    console.log("John snow has been slain")
+} else {
+    johnSnowHealth = johnSnowHealth - jamieLannisterAttack
+    console.log(`John's Health is ${johnSnowHealth}`)
+}
+
+johnSnowDefense += 25 
+
+if (johnSnowHealth<= jamieLannisterAttack - johnSnowDefense){
+    console.log ("John Snow has been slain")
+} else {
+    johnSnowHealth -= (jamieLannisterAttack - johnSnowDefense)
+    console.log(`Johns Health is ${johnSnowHealth}`)
+}
--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
-//Code:dfgdfgdf 
+//Code:
+let johnSnowAttack = 25
+let jamieLannisterAttack = 35 
+
+if (johnSnowAttack > jamieLannisterAttack) {
+    console.log("John Snow has a better Attack")
+} else {
+    console.log("Jaime Lannister has a better Attack")
+}
